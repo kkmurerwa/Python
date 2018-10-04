fc4899882964f1cea021099f0dcaa0c4ca456103
Kenneth Murerwa <xwaxes@gmail.com>
2018-10-04 10:35:18 +0300

    Add files via upload

--- kisumuwater.py
+++ kisumuwater.py
@@ -1,9 +1,3 @@
-def clityp():
-	print("Enter the number representing the type of user\n")
-	print("1.Metered\n2.Unmetered\n")
-	client=int(input())
-	return client
-
 def units():
 	unit=int(input("Enter the number of units used\n"))
 	return unit
@@ -14,26 +8,7 @@ def payinfo():
 def outp(bill):
 	print("Your total bill is: ",bill)
 
-def met():
-	unit = units()
-	if (unit<150):
-		bill = unit*33
-		if (int(payinfo())==1):
-			discount=bill*5/100
-		else:
-			discount=0;
-	outp(bill-discount)
+int unit,nav
 
-def unmet():
-	pass
 
-def main():
-	if (clityp()==1):
-		met()
-	elif (clityp()==2):
-		unmet()
-	else:
-		print("The number entered is not valid")
-		
-main();
 	
